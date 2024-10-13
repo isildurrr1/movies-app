3ac8970fa2b23fef7dc5a511d141410943a185a8
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-10-13 20:17:42 +0700

    hide my_api_key

--- src/utils/MoviesApi.ts
+++ src/utils/MoviesApi.ts
@@ -26,11 +26,12 @@ class MoviesApi {
   }
 }
 
+console.log(process.env.REACT_APP_API_TOKEN)
+
 export const moviesApi = new MoviesApi({
   baseUrl: 'https://api.themoviedb.org/3/search/movie?query=return&include_adult=false&language=en-US&page=1',
   headers: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzk0ZjQ3MjgzZDYwZTg4YzQ5OTIwZDNhOWZlYzQ2YiIsIm5iZiI6MTcyODY2NDY4OC44MjcwMzQsInN1YiI6IjY3MDkyODBjZWU5NjE0ODU4NzI0ZDgwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ctuiTQDT3c4xQR292c5DQ1dYuDzSXsw6NRt217RH10',
+    Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
   },
 })
